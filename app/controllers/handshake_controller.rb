f96a8aacccb43f98038ae6e641bf741e0f2d7530
Henrique P Linhares <linhares.h@gmail.com>
2016-11-15 22:27:56 -0200

    Serverside Handshake funcionando

--- app/controllers/handshake_controller.rb
+++ app/controllers/handshake_controller.rb
@@ -1,6 +1,17 @@
+class ApplicationController < ActionController::API
+    include ActionController::MimeResponds
+end
+
 class HandshakeController < ApplicationController
+
     # GET /handshake 
     def index
-        @message = "syn-ack:" << Rails.application.class.parent_name
+    successResponseObject = OpenStruct.new
+    successResponseObject.objectType = "handshake object"
+    successResponseObject.response = "success"
+    successResponseObject.serverName = Rails.application.class.parent_name
+        respond_to do |format|
+            format.json { render(json: successResponseObject) }
+        end
     end
 end
