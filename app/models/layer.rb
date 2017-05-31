46ca10f696d21f43a3d58b3797abf5f54d813507
Henrique do Prado Linhares <linhares.h@gmail.com>
2017-05-31 19:16:19 -0300

    Trabalhnado no cálculo das layers superiores

--- app/models/layer.rb
+++ app/models/layer.rb
@@ -1,7 +1,15 @@
 class Layer < ApplicationRecord
     has_many :reads
 
-    def addRead(pRead) 
+    def addReadToAllLayers(pRead)
+        firstLayer = Layer.find_by(id: 1)
+        firstLayer.applyPrecisionCoeficient(pRead)
+        Layer.where('id != 1').each |layer| do
+            layer.calculateLayer
+        end
+    end
+
+    def addReadToThisLayer(pRead) 
         firstRead = self.reads.find_by(latitude: pRead.latitude, longitude: pRead.longitude)
         newRead = Read.new(id: read.id, latitude: read.latitude, signalStrength: read.signalStrength, carrierName: read.carrierName)
         applyPrecisionCoeficient(read)
@@ -19,10 +27,32 @@ class Layer < ApplicationRecord
 
     end 
 
-    def applyPrecisionCoeficient(pRead)
-
+    private def applyPrecisionCoeficient(pRead)
+        latitudeInt = Integer(pRead.latitude/self.precision_coeficient)
+        pRead.latitude = Float(latitudeInt) * self.precision_coeficient
+        longitudeInt = Integer(pRead.longitude/self.precision_coeficient)
+        pRead.longitude = Float(longitudeInt) * self.precision_coeficient
         return pRead
     end
 
+    private def calculateLayer
+        firstLayerReads = firstLayer.reads
+        currentLayerReads = []
+        firstLayer
+
+        Layer.find_by(id: 1).reads do |flRead|
+            read = Read.new(id: flRead.id, latitude: flRead.latitude, signalStrength: flRead.signalStrength, carrierName: flRead.carrierName)
+            currentLayerReads.push(self.applyPrecisionCoeficient(read))
+        end
+
+        currentLayerReads = self.mergeDuplicatedReads(currentLayerReads)
+        #parei aqui
+        self.reads = currentLayerReads
+
+    end
+
+    private def mergeDuplicatedReads
+    #todo: implementar método
+    end
 
 end
