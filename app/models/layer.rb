9c1583e9d7a9e209b9def760d2d71009004f3fb6
Henrique do Prado Linhares <linhares.h@gmail.com>
2017-06-02 23:12:20 -0300

    Resolvendo os problemas em consolidação de leituras

--- app/models/layer.rb
+++ app/models/layer.rb
@@ -10,17 +10,14 @@ class Layer < ApplicationRecord
 
     def add_read_to_this_layer(pRead) 
         firstRead = self.reads.find_by(latitude: pRead.latitude, longitude: pRead.longitude)
-        puts 'chegou aqui'
         puts ::Read.all
-        newRead = ::Read.new(latitude: pRead.latitude, longitude: pRead.longitude, signalStrength: pRead.signalStrength, carrierName: pRead.carrierName)
-        newRead = applyPrecisionCoeficient(newRead)
+        newRead = ::Read.create(latitude: pRead.latitude, longitude: pRead.longitude, signalStrength: pRead.signalStrength, carrierName: pRead.carrierName)
+        newRead = apply_precision_coeficient(newRead)
 
         if (firstRead.nil?)
-            puts "Adicionando nova read na layer #{self.id}"
-            newRead.layer = self 
+            newRead.layer = Layer.find_by(id: 1)
             newRead.save
         else
-            puts "Atualizando valor de read na layer #{self.id}"
             alpha = 1
             newSignalStrengthValue = (firstRead.signalStrength*1-alpha) + (pRead.signalStrength*alpha)
             firstRead.update(signalStrength: newSignalStrengthValue)
@@ -28,7 +25,7 @@ class Layer < ApplicationRecord
 
     end 
 
-    private def applyPrecisionCoeficient(pRead)
+    def apply_precision_coeficient(pRead)
         latitudeInt = Integer(pRead.latitude/self.precision_coeficient)
         pRead.latitude = Float(latitudeInt) * self.precision_coeficient
         longitudeInt = Integer(pRead.longitude/self.precision_coeficient)
@@ -38,24 +35,44 @@ class Layer < ApplicationRecord
 
     def calculate_layer
         firstLayerReads = Layer.find_by(id: 1).reads
+        
+        puts 'firstLayerReads'
+        puts firstLayerReads
+
         currentLayerReads = []
-        Layer.find_by(id: 1).reads do |flRead|
-            read = ::Read.new(latitude: flRead.latitude, signalStrength: flRead.signalStrength, carrierName: flRead.carrierName)
-            currentLayerReads.push(self.applyPrecisionCoeficient(read))
+        firstLayerReads.each do |flRead|
+            puts 'flRead'
+            puts flRead
+            read = ::Read.create(latitude: flRead.latitude, longitude: flRead.longitude, signalStrength: flRead.signalStrength, carrierName: flRead.carrierName)
+            currentLayerReads.push(self.apply_precision_coeficient(read))
         end
+        puts '$'
+        puts currentLayerReads
+        puts '$'
+        
         self.reads = self.merge_duplicate_reads(currentLayerReads)
     end
 
     def merge_duplicate_reads(reads)
         returning_reads = []
         reads.each do |read|
-            duplicates = reads.find_by(latitude: read.latitude, longitude: read.longitude)
+            duplicates = find_duplicates(read, reads)
             if duplicates.count == 1
                 returning_reads.push(read)
             else
                 returning_reads.push(merge_duplicate_read(duplicates))
             end
         end
+    end
+
+    def find_duplicates(current, readsArr)
+        duplicates = []
+        readsArr.each do |read|
+            if (current.latitude == read.latitude && current.longitude == read.longitude)
+                duplicates.push(read)
+            end
+        end
+        return duplicates    
     end
 
     def merge_duplicate_read(duplicates)
